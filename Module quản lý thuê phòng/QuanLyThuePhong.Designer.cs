15fc4b18f04b01bd79ce208f9fd28a9c52fdb65e
maingocle <maingocle.mta@gmail.com>
2020-04-20 16:22:00 +0700

    Thuê phòng

---
+++ Module quản lý thuê phòng/QuanLyThuePhong.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace QLKhachSan.GUI.Controls
+{
+    partial class QuanLyThuePhong
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMaKhachHang = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtMaPhong = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnThemKH = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxMaPhieu = new System.Windows.Forms.TextBox();
+            this.btnThuePhong = new System.Windows.Forms.Button();
+            this.dgvThuePhong = new System.Windows.Forms.DataGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBoxLP = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listViewLP = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThuePhong)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(390, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "PHIẾU THUÊ PHÒNG";
+            // 
+            // txtMaKhachHang
+            // 
+            this.txtMaKhachHang.Location = new System.Drawing.Point(173, 28);
+            this.txtMaKhachHang.Name = "txtMaKhachHang";
+            this.txtMaKhachHang.Size = new System.Drawing.Size(176, 22);
+            this.txtMaKhachHang.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Mã Khách Hàng: ";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(390, 333);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(222, 20);
+            this.label14.TabIndex = 6;
+            this.label14.Text = "DANH SÁCH CÁC PHÒNG";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.txtMaPhong);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(516, 121);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(358, 110);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin phòng";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(116, 68);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(136, 22);
+            this.dateTimePicker1.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 33);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(85, 16);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Mã Phòng: ";
+            // 
+            // txtMaPhong
+            // 
+            this.txtMaPhong.Location = new System.Drawing.Point(116, 27);
+            this.txtMaPhong.Name = "txtMaPhong";
+            this.txtMaPhong.Size = new System.Drawing.Size(136, 22);
+            this.txtMaPhong.TabIndex = 10;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(13, 68);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(96, 16);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "Ngày Thuê:  ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnThemKH);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtMaKhachHang);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(516, 10);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(358, 105);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thông tin khách hàng";
+            // 
+            // btnThemKH
+            // 
+            this.btnThemKH.Location = new System.Drawing.Point(266, 61);
+            this.btnThemKH.Name = "btnThemKH";
+            this.btnThemKH.Size = new System.Drawing.Size(83, 32);
+            this.btnThemKH.TabIndex = 13;
+            this.btnThemKH.Text = "Thêm KH";
+            this.btnThemKH.UseVisualStyleBackColor = true;
+            this.btnThemKH.Click += new System.EventHandler(this.btnThemKH_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.textBoxMaPhieu);
+            this.panel1.Controls.Add(this.btnThuePhong);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Location = new System.Drawing.Point(19, 34);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(887, 281);
+            this.panel1.TabIndex = 9;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // textBoxMaPhieu
+            // 
+            this.textBoxMaPhieu.Location = new System.Drawing.Point(355, 248);
+            this.textBoxMaPhieu.Name = "textBoxMaPhieu";
+            this.textBoxMaPhieu.Size = new System.Drawing.Size(176, 20);
+            this.textBoxMaPhieu.TabIndex = 14;
+            // 
+            // btnThuePhong
+            // 
+            this.btnThuePhong.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnThuePhong.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThuePhong.ForeColor = System.Drawing.Color.Red;
+            this.btnThuePhong.Location = new System.Drawing.Point(568, 238);
+            this.btnThuePhong.Name = "btnThuePhong";
+            this.btnThuePhong.Size = new System.Drawing.Size(100, 36);
+            this.btnThuePhong.TabIndex = 9;
+            this.btnThuePhong.Text = "Thuê Phòng";
+            this.btnThuePhong.UseVisualStyleBackColor = false;
+            this.btnThuePhong.Click += new System.EventHandler(this.btnThuePhong_Click_1);
+            // 
+            // dgvThuePhong
+            // 
+            this.dgvThuePhong.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvThuePhong.Location = new System.Drawing.Point(19, 369);
+            this.dgvThuePhong.Name = "dgvThuePhong";
+            this.dgvThuePhong.Size = new System.Drawing.Size(887, 106);
+            this.dgvThuePhong.TabIndex = 10;
+            this.dgvThuePhong.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvThuePhong_CellContentClick_1);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.listViewLP);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.comboBoxLP);
+            this.groupBox3.Location = new System.Drawing.Point(32, 15);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(436, 216);
+            this.groupBox3.TabIndex = 15;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Kiểm tra thông tin phòng";
+            // 
+            // comboBoxLP
+            // 
+            this.comboBoxLP.FormattingEnabled = true;
+            this.comboBoxLP.Location = new System.Drawing.Point(105, 24);
+            this.comboBoxLP.Name = "comboBoxLP";
+            this.comboBoxLP.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxLP.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Loại phòng";
+            // 
+            // listViewLP
+            // 
+            this.listViewLP.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listViewLP.Location = new System.Drawing.Point(42, 67);
+            this.listViewLP.Name = "listViewLP";
+            this.listViewLP.Size = new System.Drawing.Size(297, 129);
+            this.listViewLP.TabIndex = 2;
+            this.listViewLP.UseCompatibleStateImageBehavior = false;
+            this.listViewLP.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "STT";
+            this.columnHeader1.Width = 40;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Mã phòng";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Tên phòng";
+            this.columnHeader3.Width = 70;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Giá";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Sức chứa";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(264, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Tìm kiếm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(255, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 19);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Mã phiếu";
+            // 
+            // QuanLyThue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(950, 497);
+            this.Controls.Add(this.dgvThuePhong);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label1);
+            this.Name = "QuanLyThue";
+            this.Load += new System.EventHandler(this.QuanLyThue_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThuePhong)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMaKhachHang;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtMaPhong;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnThemKH;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnThuePhong;
+        private System.Windows.Forms.TextBox textBoxMaPhieu;
+        private System.Windows.Forms.DataGridView dgvThuePhong;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListView listViewLP;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxLP;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Module quản lý thuê phòng/QuanLyThuePhong.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QLKhachSan.GUI.Controls;
+using QLKhachSan.Model;
+using System.Data.Entity.Migrations;
+
+namespace QLKhachSan.GUI.Controls
+{
+    public partial class QuanLyThuePhong :Form
+    {
+        private QUANLYKHACHSANEntities db = new QUANLYKHACHSANEntities();
+        private static List<string> Selected = new List<string>();
+      
+
+        public QuanLyThuePhong()
+        {
+            InitializeComponent();
+            setCbbDichVu();
+           // HienThiDanhSach();
+         
+        }
+        List<string> getAllLP()
+        {
+            List<string> lp = new List<string>();
+            lp = db.LOAIPHONGs.Select(x => x.TenLoaiPhong).ToList();
+            return lp;
+        }
+        private void setCbbDichVu()
+        {
+            List<string> dV = getAllLP();
+            foreach (string temp in dV)
+            {
+                comboBoxLP.Items.Add(temp);
+            }
+        }
+        public void HienThiDanhSach()
+        {
+            listViewLP.Items.Clear();
+            int stt = 1;
+            var lstPhongThue = (from phong in db.PHONGs
+                                join lp in db.LOAIPHONGs on phong.MaLoaiPhong equals lp.MaLoaiPhong
+                               
+                                 where phong.TrangThai.Contains("Trống") && lp.TenLoaiPhong.Contains(comboBoxLP.Text)
+                                select new
+                                {
+                                    MAPHONG=phong.MaPhong,
+                                    TENPHONG=phong.SoPhong,
+                                    GIA=lp.GiaPhong,
+                                    SONGUOI=lp.SoNgToiDa,
+                                    
+                                }).ToList();
+            foreach (var phongThue in lstPhongThue)
+            {
+                string[] row =
+                    {
+                            stt.ToString(), phongThue.MAPHONG, phongThue.TENPHONG.ToString(), phongThue.GIA.ToString(),
+                            phongThue.SONGUOI.ToString()
+                        };
+                ListViewItem item = new ListViewItem(row);
+                stt++;
+                listViewLP.Items.Add(item);
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HienThiDanhSach();
+
+        }
+
+        void loadDataToGridView(DataGridView dtgv)
+        {
+            using (QUANLYKHACHSANEntities db = new QUANLYKHACHSANEntities())
+            {
+
+
+                var query =
+                             from p in db.PHONGs
+                             join sd in db.PHIEUTHUEPHONGs on p.MaPhong equals sd.MaPhong
+                             join kh in db.KHACHHANGs on sd.MaKH equals kh.MaKH
+                            // join hd in db.HOADONs on kh.MaKH equals hd.MaKH
+                             where p.TrangThai.Contains("Đã ở")
+                             select new
+                             {
+                                 maPhong = p.MaPhong,
+                                 maKH = sd.MaKH,
+                                 diaChi = kh.DiaChi,
+                                 tenPhong = p.SoPhong,
+                                 ngayThue = sd.NgayNhan,
+                                 tenKH = kh.TenKH,
+                                 trangThai = p.TrangThai,
+                             };
+   
+                var results = query.ToList();
+                dtgv.DataSource = results;
+
+
+
+            }
+           
+
+        }
+
+        private void txtMaPhong_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTenPhong_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+       
+       
+        private void btnThemKH_Click_1(object sender, EventArgs e)
+        {
+            QuanLyKhachHang themKhachHang = new QuanLyKhachHang();
+            themKhachHang.ShowDialog();
+        }
+
+        private void btnThuePhong_Click_1(object sender, EventArgs e)
+        {
+           
+                PHIEUTHUEPHONG sd = new PHIEUTHUEPHONG()
+                {
+                        MaPhieu = textBoxMaPhieu.Text,
+                        MaPhong = txtMaPhong.Text,
+                        MaKH = txtMaKhachHang.Text,
+                        NgayNhan = DateTime.Now
+                    };
+                    db.PHIEUTHUEPHONGs.Add(sd);
+
+                   string MaPhong = txtMaPhong.Text;
+                   var phong = (from p in db.PHONGs where p.MaPhong.Equals(MaPhong) select p).FirstOrDefault();
+                   phong.TrangThai = "Đã ở";
+                   db.PHONGs.AddOrUpdate(phong);
+
+            
+                       
+                       
+                    
+                   db.SaveChanges();
+                   
+                    MessageBox.Show("Thêm thành công !");
+                    loadDataToGridView(dgvThuePhong);
+        }
+                
+            
+        int index;
+        private void dgvThuePhong_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            index = dgvThuePhong.CurrentRow.Index;
+           // stt=  dgvThuePhong.Rows[index].Cells[0].Value.ToString();
+          
+            txtMaKhachHang.Text = dgvThuePhong.Rows[index].Cells[1].Value.ToString();
+
+           
+            txtMaPhong.Text= dgvThuePhong.Rows[index].Cells[3].Value.ToString();
+            
+            dateTimePicker1.Text= dgvThuePhong.Rows[index].Cells[5].Value.ToString();
+            
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void QuanLyThue_Load(object sender, EventArgs e)
+        {
+            loadDataToGridView(dgvThuePhong);
+        }
+
+        private void dgvThuePhong_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            index = dgvThuePhong.CurrentRow.Index;
+            // stt=  dgvThuePhong.Rows[index].Cells[0].Value.ToString();
+           // txtTenKhachHang.Text = dgvThuePhong.Rows[index].Cells[0].Value.ToString();
+            txtMaKhachHang.Text = dgvThuePhong.Rows[index].Cells[1].Value.ToString();
+
+           // txtDiaChi.Text = dgvThuePhong.Rows[index].Cells[2].Value.ToString();
+            txtMaPhong.Text = dgvThuePhong.Rows[index].Cells[3].Value.ToString();
+           // txtTenPhong.Text = dgvThuePhong.Rows[index].Cells[4].Value.ToString();
+            dateTimePicker1.Text = dgvThuePhong.Rows[index].Cells[5].Value.ToString();
+           // txtTrangThai.Text = dgvThuePhong.Rows[index].Cells[6].Value.ToString();
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+
+}
