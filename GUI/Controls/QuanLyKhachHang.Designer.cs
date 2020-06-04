68bdfe5263aae5ea1097a9056f13bf4682b2e7df
tranhuyen123 <tranhuyenat9@gmail.com>
2020-06-04 21:53:26 +0700

    Quản lý khách hàng

---
+++ GUI/Controls/QuanLyKhachHang.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace QLKhachSan.GUI.Controls
+{
+    partial class QuanLyKhachHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBoxGT = new System.Windows.Forms.ComboBox();
+            this.textBoxDC = new System.Windows.Forms.TextBox();
+            this.textBoxCMND = new System.Windows.Forms.TextBox();
+            this.textBoxSDT = new System.Windows.Forms.TextBox();
+            this.textBoxTenKH = new System.Windows.Forms.TextBox();
+            this.textBoxMaKH = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonThoat = new System.Windows.Forms.Button();
+            this.buttonXoa = new System.Windows.Forms.Button();
+            this.buttonSua = new System.Windows.Forms.Button();
+            this.buttonThem = new System.Windows.Forms.Button();
+            this.dataGridViewKH = new System.Windows.Forms.DataGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBoxTK = new System.Windows.Forms.TextBox();
+            this.buttonTK = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewKH)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(292, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quản lý khách hàng";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBoxGT);
+            this.groupBox1.Controls.Add(this.textBoxDC);
+            this.groupBox1.Controls.Add(this.textBoxCMND);
+            this.groupBox1.Controls.Add(this.textBoxSDT);
+            this.groupBox1.Controls.Add(this.textBoxTenKH);
+            this.groupBox1.Controls.Add(this.textBoxMaKH);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(57, 98);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(408, 156);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Khách hàng";
+            // 
+            // comboBoxGT
+            // 
+            this.comboBoxGT.FormattingEnabled = true;
+            this.comboBoxGT.Items.AddRange(new object[] {
+            "Nam",
+            "Nữ"});
+            this.comboBoxGT.Location = new System.Drawing.Point(284, 75);
+            this.comboBoxGT.Name = "comboBoxGT";
+            this.comboBoxGT.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxGT.TabIndex = 11;
+            // 
+            // textBoxDC
+            // 
+            this.textBoxDC.Location = new System.Drawing.Point(284, 115);
+            this.textBoxDC.Name = "textBoxDC";
+            this.textBoxDC.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDC.TabIndex = 10;
+            // 
+            // textBoxCMND
+            // 
+            this.textBoxCMND.Location = new System.Drawing.Point(284, 36);
+            this.textBoxCMND.Name = "textBoxCMND";
+            this.textBoxCMND.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCMND.TabIndex = 9;
+            // 
+            // textBoxSDT
+            // 
+            this.textBoxSDT.Location = new System.Drawing.Point(105, 119);
+            this.textBoxSDT.Name = "textBoxSDT";
+            this.textBoxSDT.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSDT.TabIndex = 8;
+            // 
+            // textBoxTenKH
+            // 
+            this.textBoxTenKH.Location = new System.Drawing.Point(105, 75);
+            this.textBoxTenKH.Name = "textBoxTenKH";
+            this.textBoxTenKH.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTenKH.TabIndex = 7;
+            // 
+            // textBoxMaKH
+            // 
+            this.textBoxMaKH.Location = new System.Drawing.Point(105, 36);
+            this.textBoxMaKH.Name = "textBoxMaKH";
+            this.textBoxMaKH.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMaKH.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(224, 124);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Địa chỉ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(217, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Giới tính";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(224, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "CMND";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Số điện thoại";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Họ tên ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mã khách hàng";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.buttonThoat);
+            this.groupBox2.Controls.Add(this.buttonXoa);
+            this.groupBox2.Controls.Add(this.buttonSua);
+            this.groupBox2.Controls.Add(this.buttonThem);
+            this.groupBox2.Location = new System.Drawing.Point(484, 98);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(267, 156);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chức năng";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Location = new System.Drawing.Point(142, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonThoat
+            // 
+            this.buttonThoat.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.buttonThoat.Location = new System.Drawing.Point(51, 88);
+            this.buttonThoat.Name = "buttonThoat";
+            this.buttonThoat.Size = new System.Drawing.Size(75, 23);
+            this.buttonThoat.TabIndex = 3;
+            this.buttonThoat.Text = "Thoát";
+            this.buttonThoat.UseVisualStyleBackColor = false;
+            this.buttonThoat.Click += new System.EventHandler(this.buttonThoat_Click);
+            // 
+            // buttonXoa
+            // 
+            this.buttonXoa.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.buttonXoa.Location = new System.Drawing.Point(184, 45);
+            this.buttonXoa.Name = "buttonXoa";
+            this.buttonXoa.Size = new System.Drawing.Size(75, 23);
+            this.buttonXoa.TabIndex = 2;
+            this.buttonXoa.Text = "Xóa";
+            this.buttonXoa.UseVisualStyleBackColor = false;
+            this.buttonXoa.Click += new System.EventHandler(this.buttonXoa_Click);
+            // 
+            // buttonSua
+            // 
+            this.buttonSua.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.buttonSua.Location = new System.Drawing.Point(103, 45);
+            this.buttonSua.Name = "buttonSua";
+            this.buttonSua.Size = new System.Drawing.Size(75, 23);
+            this.buttonSua.TabIndex = 1;
+            this.buttonSua.Text = "Sửa";
+            this.buttonSua.UseVisualStyleBackColor = false;
+            this.buttonSua.Click += new System.EventHandler(this.buttonSua_Click);
+            // 
+            // buttonThem
+            // 
+            this.buttonThem.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.buttonThem.Location = new System.Drawing.Point(22, 45);
+            this.buttonThem.Name = "buttonThem";
+            this.buttonThem.Size = new System.Drawing.Size(75, 23);
+            this.buttonThem.TabIndex = 0;
+            this.buttonThem.Text = "Thêm";
+            this.buttonThem.UseVisualStyleBackColor = false;
+            this.buttonThem.Click += new System.EventHandler(this.buttonThem_Click);
+            // 
+            // dataGridViewKH
+            // 
+            this.dataGridViewKH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewKH.Location = new System.Drawing.Point(30, 19);
+            this.dataGridViewKH.Name = "dataGridViewKH";
+            this.dataGridViewKH.Size = new System.Drawing.Size(641, 134);
+            this.dataGridViewKH.TabIndex = 0;
+            this.dataGridViewKH.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewKH_CellContentClick);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dataGridViewKH);
+            this.groupBox3.Location = new System.Drawing.Point(57, 279);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(694, 159);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Thông tin khách hàng";
+            // 
+            // textBoxTK
+            // 
+            this.textBoxTK.Location = new System.Drawing.Point(262, 57);
+            this.textBoxTK.Name = "textBoxTK";
+            this.textBoxTK.Size = new System.Drawing.Size(241, 20);
+            this.textBoxTK.TabIndex = 4;
+            // 
+            // buttonTK
+            // 
+            this.buttonTK.Location = new System.Drawing.Point(520, 54);
+            this.buttonTK.Name = "buttonTK";
+            this.buttonTK.Size = new System.Drawing.Size(75, 23);
+            this.buttonTK.TabIndex = 5;
+            this.buttonTK.Text = "Tìm kiếm";
+            this.buttonTK.UseVisualStyleBackColor = true;
+            this.buttonTK.Click += new System.EventHandler(this.buttonTK_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(176, 64);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Nhập tên";
+            // 
+            // QuanLyKhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 464);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.buttonTK);
+            this.Controls.Add(this.textBoxTK);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "QuanLyKhachHang";
+            this.Text = "QuanLyKhachHang";
+            this.Load += new System.EventHandler(this.QuanLyKhachHang_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewKH)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBoxGT;
+        private System.Windows.Forms.TextBox textBoxDC;
+        private System.Windows.Forms.TextBox textBoxCMND;
+        private System.Windows.Forms.TextBox textBoxSDT;
+        private System.Windows.Forms.TextBox textBoxTenKH;
+        private System.Windows.Forms.TextBox textBoxMaKH;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonThoat;
+        private System.Windows.Forms.Button buttonXoa;
+        private System.Windows.Forms.Button buttonSua;
+        private System.Windows.Forms.Button buttonThem;
+        private System.Windows.Forms.DataGridView dataGridViewKH;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBoxTK;
+        private System.Windows.Forms.Button buttonTK;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ GUI/Controls/QuanLyKhachHang.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QLKhachSan.Model;
+namespace QLKhachSan.GUI.Controls
+{
+    public partial class QuanLyKhachHang : Form
+    {
+        public QuanLyKhachHang()
+        {
+            InitializeComponent();
+        }
+        void LoadKH(DataGridView dtgv)
+        {
+            using (QUANLYKHACHSANEntities db = new QUANLYKHACHSANEntities())
+            {
+                var query = from kh in db.KHACHHANGs
+                            select new
+                            {
+                                Mã_KH=kh.MaKH,
+                                Tên_KH=kh.TenKH,
+                                SĐT= kh.SDT,
+                                CMND=kh.CMND,
+                                Địa_Chỉ=kh.DiaChi,
+                                Giới_Tính=kh.GioiTinh,
+
+                            };
+                var result = query.ToList();
+                dtgv.DataSource = result;
+
+            }
+            BindingKH(dataGridViewKH);
+        }
+        void BindingKH(DataGridView dtgv)
+        {
+            Binding bdMa = new Binding("text", dtgv.DataSource, "Mã_KH", true, DataSourceUpdateMode.OnPropertyChanged);
+            textBoxMaKH.DataBindings.Clear();
+            textBoxMaKH.DataBindings.Add(bdMa);
+
+            Binding bdTen = new Binding("text", dtgv.DataSource, "Tên_KH", true, DataSourceUpdateMode.OnPropertyChanged);
+            textBoxTenKH.DataBindings.Clear();
+            textBoxTenKH.DataBindings.Add(bdTen);
+
+            Binding bdSDT = new Binding("text", dtgv.DataSource, "SĐT", true, DataSourceUpdateMode.OnPropertyChanged);
+            textBoxSDT.DataBindings.Clear();
+            textBoxSDT.DataBindings.Add(bdSDT);
+
+            Binding bdCMND = new Binding("text", dtgv.DataSource, "CMND", true, DataSourceUpdateMode.OnPropertyChanged);
+            textBoxCMND.DataBindings.Clear();
+            textBoxCMND.DataBindings.Add(bdCMND);
+            Binding bdGT = new Binding("text", dtgv.DataSource, "Giới_Tính", true, DataSourceUpdateMode.OnPropertyChanged);
+            comboBoxGT.DataBindings.Clear();
+            comboBoxGT.DataBindings.Add(bdGT);
+
+            Binding bdDC = new Binding("text", dtgv.DataSource, "Địa_Chỉ", true, DataSourceUpdateMode.OnPropertyChanged);
+            textBoxDC.DataBindings.Clear();
+            textBoxDC.DataBindings.Add(bdDC);
+        
+            
+        }
+
+        private void QuanLyKhachHang_Load(object sender, EventArgs e)
+        {
+            LoadKH(dataGridViewKH);
+        }
+
+        private void buttonThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonXoa_Click(object sender, EventArgs e)
+        {
+            using(QUANLYKHACHSANEntities db=new QUANLYKHACHSANEntities())
+            {
+                db.KHACHHANGs.Remove(db.KHACHHANGs.Find(Int32.Parse(textBoxMaKH.Text)));
+                db.SaveChanges();
+                MessageBox.Show("Xóa thành công");
+                LoadKH(dataGridViewKH);
+            }
+            
+        }
+
+        private void buttonThem_Click(object sender, EventArgs e)
+        {
+            using (QUANLYKHACHSANEntities db = new QUANLYKHACHSANEntities())
+            {
+                KHACHHANG kh = new KHACHHANG()
+                {
+                    MaKH= textBoxMaKH.Text,
+                    TenKH=textBoxTenKH.Text,
+                    SDT=textBoxSDT.Text,
+                    CMND=textBoxCMND.Text,
+                    GioiTinh = comboBoxGT.Text,
+                    DiaChi =textBoxDC.Text,
+
+                   
+
+
+
+                };
+                db.KHACHHANGs.Add(kh);
+                MessageBox.Show("Thêm thành công");
+                db.SaveChanges();
+                LoadKH(dataGridViewKH);
+            }
+
+        }
+
+        private void buttonSua_Click(object sender, EventArgs e)
+        {
+            using (QUANLYKHACHSANEntities db = new QUANLYKHACHSANEntities())
+            {
+                KHACHHANG kh = db.KHACHHANGs.Find(Int32.Parse(textBoxMaKH.Text));
+                kh.TenKH = textBoxTenKH.Text;
+                kh.SDT = textBoxSDT.Text;
+                kh.CMND = textBoxCMND.Text;
+                kh.GioiTinh = comboBoxGT.Text;
+                kh.DiaChi = textBoxDC.Text;
+                db.SaveChanges();
+                MessageBox.Show("Sửa thành công");
+                LoadKH(dataGridViewKH);
+
+            }
+
+        }
+        int index;
+        private void dataGridViewKH_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            index = dataGridViewKH.CurrentRow.Index;
+            textBoxMaKH.Text = dataGridViewKH.Rows[index].Cells[0].Value.ToString();
+            textBoxTenKH.Text= dataGridViewKH.Rows[index].Cells[1].Value.ToString();
+            textBoxSDT.Text= dataGridViewKH.Rows[index].Cells[2].Value.ToString();
+            textBoxCMND.Text= dataGridViewKH.Rows[index].Cells[3].Value.ToString();
+            comboBoxGT.Text= dataGridViewKH.Rows[index].Cells[4].Value.ToString();
+            textBoxDC.Text= dataGridViewKH.Rows[index].Cells[5].Value.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxMaKH.Text = "";
+            textBoxTenKH.Text = "";
+            textBoxSDT.Text = "";
+            textBoxCMND.Text = "";
+            textBoxDC.Text = "";
+        }
+        void LoadTK(DataGridView dtgv)
+        {
+            using (QUANLYKHACHSANEntities db = new QUANLYKHACHSANEntities())
+            {
+                var query = from kh in db.KHACHHANGs
+                            where kh.TenKH.Contains(textBoxTK.Text)
+                            select new
+                            {
+                                Mã_KH = kh.MaKH,
+                                Tên_KH = kh.TenKH,
+                                SĐT = kh.SDT,
+                                CMND = kh.CMND,
+                                Địa_Chỉ = kh.DiaChi,
+                                Giới_Tính = kh.GioiTinh,
+
+                            };
+                var result = query.ToList();
+                dtgv.DataSource = result;
+
+            }
+            BindingKH(dataGridViewKH);
+        }
+        private void buttonTK_Click(object sender, EventArgs e)
+        {
+            LoadTK(dataGridViewKH);
+        }
+    }
+}
