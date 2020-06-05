36ec217d95b3020b78e12ef4e70493ac02f6cb7c
tranhuyen123 <tranhuyenat9@gmail.com>
2020-06-05 10:07:05 +0700

    chạy

---
+++ Common.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLKhachSan
+{
+    class Common
+    {
+        public static List<string> SelectedPhong = new List<string>();
+    }
+}
